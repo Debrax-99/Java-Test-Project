c51c87986bf4747f9b16f8c274f5ec971991dd1f
Debrax-99 <am.blan.esc@gmail.com>
2020-04-24 15:26:46 +0200

    Refactoring and comments on static methods

--- src/operations/Helper.java
+++ src/operations/Helper.java
@@ -6,52 +6,69 @@ import model.Perro;
 
 public class Helper {
 	
-	public static double calculoAntibioticos (final Perro perro) {
+	//CALCULATE ANTIBIOTIC DOSE
+	public static double calculoAntibioticos(final Perro perro) {
 		//Formula antibioticos: 12'5 mg de sustancia activa por kg de peso.
 		final double antibioticos = perro.getPeso() * 12.5;
 		return antibioticos;
 	}
 	
-	public static int calculoPienso (final Perro perro) {
+	//CALCULATE DAILY FEED AMOUNT
+	public static int calculoPienso(final Perro perro) {
 		//Cantidad pienso segun edad y peso, para cachorros, adultos y seniors.
 		int pienso = 0;
+		//Comprobamos elrango de edad del perro, y ajustamos cantidad segun peso.
 		if ((perro.getEdad() >= 1) && (perro.getEdad() < 9)) {
-			if (perro.getPeso() <= 5) {
-				pienso = 40;
-			} else {
-				if (perro.getPeso() <= 12) {
-					pienso = 130;
-				} else {
-					if (perro.getPeso() <= 25) {
-						pienso = 400;
-					} else {
-						if (perro.getPeso() <= 45) {
-							pienso = 700;
-						} else {
-							pienso = 1000;
-						}
-					}
-				}
-			}
+			pienso = piensoAdultos(perro);
 		} else {
 			if (perro.getEdad() < 1) {
 				pienso = 25;
 			} else {
-				if (perro.getPeso() <= 5) {
-					pienso = 35;
+				pienso = piensoSeniors(perro);
+			}
+		}
+		return pienso;
+	}
+
+	//Cantidad de pienso por kilo de peso para adultos
+	private static int piensoAdultos(final Perro perro) {
+		int pienso;
+		if (perro.getPeso() <= 5) {
+			pienso = 40;
+		} else {
+			if (perro.getPeso() <= 12) {
+				pienso = 130;
+			} else {
+				if (perro.getPeso() <= 25) {
+					pienso = 400;
 				} else {
-					if (perro.getPeso() <= 12) {
-						pienso = 100;
+					if (perro.getPeso() <= 45) {
+						pienso = 700;
 					} else {
-						if (perro.getPeso() <= 25) {
-							pienso = 350;
-						} else {
-							if (perro.getPeso() <= 45) {
-								pienso = 550;
-							} else {
-								pienso = 800;
-							}
-						}
+						pienso = 1000;
+					}
+				}
+			}
+		}
+		return pienso;
+	}
+	
+	//Cantidad de pienso por kilo de peso para seniors
+	private static int piensoSeniors(final Perro perro) {
+		int pienso;
+		if (perro.getPeso() <= 5) {
+			pienso = 35;
+		} else {
+			if (perro.getPeso() <= 12) {
+				pienso = 100;
+			} else {
+				if (perro.getPeso() <= 25) {
+					pienso = 350;
+				} else {
+					if (perro.getPeso() <= 45) {
+						pienso = 550;
+					} else {
+						pienso = 800;
 					}
 				}
 			}
@@ -59,7 +76,8 @@ public class Helper {
 		return pienso;
 	}
 
-	public static double calculoEdadHumana (final Perro perro) {
+	//CALCULATE AGE ON HUMAN YEARS
+	public static double calculoEdadHumana(final Perro perro) {
 		//Formula de equivalencia de edad perro-humano: ln de la edad por 16 mas 31, siempre que tenga mas de 1 anio.
 		double edadHumana = 0;
 		if (perro.getEdad() > 1) {
@@ -70,19 +88,19 @@ public class Helper {
 		return edadHumana;
 	}
 	
-	public static boolean comprobarVacuna (final Perro perro) {
+	//CHECK VACCINE SCHEDULE STATUS
+	public static boolean comprobarVacuna(final Perro perro) {
 		//Si el anio de ultima vacuna no coincide con el del sistema, la vacuna está desactualizada y ha de vacunarse.
 		LocalDateTime now = LocalDateTime.now();
 		boolean vacuna = false;
 		if (perro.getAnioVacuna() == now.getYear()) {
 			vacuna = true;
-		}
-		
+		}		
 		return vacuna;
 	}
 	
-	//METODO REDONDEO DOS DECIMALES:
-	public static double redondeoDos (final double num) {
+	//ROUNDING TWO DECIMALS
+	private static double redondeoDos(final double num) {
 		final double numDec = Math.round(Math.pow(10,2)*num)/Math.pow(10,2);
 		return numDec;
 	}
